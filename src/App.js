6a10630fa3d9bd1589934b83bcb53f940f6e983d
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-04 10:37:57 +0500

    todo component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
       <Header />
+      <Todo task="Learn React" deadline="12-02-2020" status={false} />
     </div>
   );
 }
