ad1fa647cd8a8e0cd999f3b468f08a0a2a919fcd
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-04 10:24:22 +0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+        <header className='header'>
+            <img src="/logo512.png" width={50}></img>
+            <h1>Project 1 - Todo List</h1>
+        </header>
+    </>
+  )
+}
