b0406163210827250e5b17417511a1989c7abdaf
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-04 10:59:10 +0500

    todo-list has been added

---
+++ src/data/todos.js
@@ -0,0 +1,20 @@
+export const todos = [
+    {
+        id: 1,
+        task: 'Learn React',
+        deadline: '12-12-2022',
+        statue: false
+    },
+    {
+        id: 2,
+        task: 'Finish the project',
+        deadline: '12-02-2030',
+        status: true
+    },
+    {
+        id: 3,
+        task: 'Complete portfolio',
+        deadline: '12-02-2030',
+        status: true
+    }
+]
