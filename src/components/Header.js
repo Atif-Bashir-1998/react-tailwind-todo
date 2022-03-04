415a43e249f195296b0689588cc56fe28b9f6f3f
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-04 10:30:13 +0500

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-        <header className='header'>
+        <header className='bg-gray-400 flex items-center justify-between p-8'>
             <img src="/logo512.png" width={50}></img>
-            <h1>Project 1 - Todo List</h1>
+            <h1 className='text-gray-600 text-3xl'>Project 1 - Todo List</h1>
         </header>
     </>
   )
