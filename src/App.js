5d63199cdc49525b0ce82e1cb490a9d22d758548
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-06 23:18:38 +0500

    added todo is visible in list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Header from './components/Header';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
-import {todos} from './data/todos';
+// import {todos} from './data/todos';
 // import Todo from './components/Todo';
+
+const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  REMOVE_TODO: 'remove-todo',
+  TOGGLE_TODO: 'toggle-todo'
+}
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      console.log(action.payload.todo)
+      return [...todos, action.payload.todo]
+    default:
+      break;
+  }
+}
 
 function App() {
   const addTodo = (todo) => {
-    console.log("hello", todo)
+    todo = {...todo, id: +new Date()}
+    dispatch({type: ACTIONS.ADD_TODO, payload: {todo}})
   }
+
+  const [todos, dispatch] = useReducer(reducer, [])
   
   return (
     <div>
