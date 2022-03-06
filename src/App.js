5df0cab9792bb6fe15a0e6ab2f55d8f984a41cc5
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-06 22:59:59 +0500

    data from the form is recieved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from './components/Header';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 import {todos} from './data/todos';
 // import Todo from './components/Todo';
 
 function App() {
+  const addTodo = (todo) => {
+    console.log("hello", todo)
+  }
+  
   return (
     <div>
       <Header />
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
     </div>
   );
